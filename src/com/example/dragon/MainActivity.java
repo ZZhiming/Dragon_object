25a8a7a177f230b9eeab12c295b0ac9cfed9fceb
ZZhiming <zhangzm05@gmail.com>
2015-04-10 18:30:22 -0500

    dragon testing

---
+++ src/com/example/dragon/MainActivity.java
@@ -0,0 +1,54 @@
+package com.example.dragon;
+
+import android.os.Bundle;
+import android.app.Activity;
+import android.content.Intent;
+import android.view.Menu;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+public class MainActivity extends Activity {
+    double x;
+	@Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Toast.makeText(this, "Welcome!!", Toast.LENGTH_SHORT).show();
+        final Button button1 = (Button) findViewById(R.id.button1);
+        button1.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                  x=1.1;
+            }
+        });  
+
+        final Button button2 = (Button) findViewById(R.id.button2);
+        button2.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                checkDouble();   
+            	x=2.2;
+            }
+        });  
+ 
+        final Button button3 = (Button) findViewById(R.id.button3);
+        button3.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                  x=3.3;
+            }
+        });  
+
+        final Button button4 = (Button) findViewById(R.id.button4);
+        button4.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                 x=4.4;
+            }
+        });  
+
+    }
+    
+    public void checkDouble(){
+    	     if(x==3.3)
+    	    	 System.out.println("Passed the double type test!");
+    }
+    
+}
